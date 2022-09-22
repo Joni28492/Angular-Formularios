2de1db34ed617120ae9b68eb1232d17694644eca
Joni <joni28492@gmail.com>
2022-09-22 08:37:12 -0400

    first commit

---
+++ src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [
+  ]
+})
+export class BasicosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  guardar(miFormulario:NgForm){
+    console.log(miFormulario.value)
+  }
+
+}
